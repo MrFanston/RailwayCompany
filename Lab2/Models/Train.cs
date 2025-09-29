1a94e35350aa99146baa804621b91c0bc9b8aaa7
Vladimir <volodya_kuznetsov_04@mail.ru>
2025-09-30 00:05:59 +0500

    Добавьте файлы проекта.

---
+++ Lab2/Models/Train.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab2.Models
+{
+    public class Train
+    {
+        public int Id { get; set; } // Primary Key
+
+        [Required]
+        [Display(Name = "Номер поезда")]
+        public string TrainNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Станция назначения")]
+        public string Destination { get; set; }
+
+        [Display(Name = "Время отправления")]
+        public DateTime DepartureTime { get; set; }
+
+        [Display(Name = "Время в пути (часы)")]
+        public double TravelTimeHours { get; set; }
+
+        [Display(Name = "Время прибытия")]
+        public DateTime ArrivalTime { get; set; }
+
+        [Display(Name = "Наличие билетов")]
+        public bool TicketsAvailable { get; set; }
+
+        [Display(Name = "Количество вагонов")]
+        public int CarriageCount { get; set; }
+    }
+}
